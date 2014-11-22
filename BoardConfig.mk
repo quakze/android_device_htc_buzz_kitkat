7960a90771613cbbe7f06e0bb5b7a889a294b249
root <cciers2011@gmail.com>
2014-11-22 09:09:56 -0800

    first commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,31 +1,168 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# throughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
 -include vendor/htc/buzz/BoardConfigVendor.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/htc/buzz/include
+
+# ARMv6-compatible processor rev 5 (v6l)
+TARGET_BOARD_PLATFORM := msm7k
 TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi-v7a
+TARGET_ARCH_VARIANT := armv6j
+TARGET_CPU_ABI := armeabi-v6j
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+BOARD_LDPI_RECOVERY := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
 
 TARGET_BOOTLOADER_BOARD_NAME := buzz
+TARGET_OTA_ASSERT_DEVICE := buzz
+TARGET_BOARD_INFO_FILE := device/htc/buzz/board-info.txt
 
-BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
-BOARD_KERNEL_BASE := 0x02e00000
-BOARD_KERNEL_PAGESIZE := 2048
+### Wifi related defines
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WLAN_DEVICE                := bcm4329
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
+#WIFI_DRIVER_FW_PATH_PARAM       := "/sys/module/bcm4329/parameters/firmware_path"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_NAME          := "bcm4329"
+WIFI_DRIVER_MODULE_ARG           := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration iface_name=wlan0"
+BOARD_WLAN_DEVICE_REV            := bcm4329
+WIFI_BAND                        := 802_11_ABG
 
-TARGET_PREBUILT_KERNEL := device/htc/buzz/kernel
+## kernel
+TARGET_KERNEL_CONFIG   		:= buzz_defconfig
+# TARGET_PREBUILT_KERNEL 	:= device/htc/buzz/prebuilt/kernel
+TARGET_RECOVERY_INITRC 		:= device/htc/buzz/initramfs/init.recovery.rc
+TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/buzz/prebuilt/recovery_kernel
 
-BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_KERNEL_CMDLINE 		:= no_console_suspend=1 console=null
+BOARD_KERNEL_BASE 		:= 0x02E00000
+BOARD_KERNEL_PAGESIZE 		:= 2048
+
+# Enable gsm and network search
+BOARD_USES_LEGACY_RIL := true
+BOARD_USE_NEW_LIBRIL_HTC := true
+
+## Buzz graphics
+BOARD_NO_RGBX_8888 := true
+BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+TARGET_FORCE_CPU_UPLOAD 	   := true
+USE_OPENGL_RENDERER   		   := false
+BOARD_EGL_CFG 	      		   := device/htc/buzz/egl.cfg
+
+BOARD_USE_FROYO_LIBCAMERA := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT -DICS_CAMERA_BLOB -DQCOM_NO_SECURE_PLAYBACK
+TARGET_DISABLE_ARM_PIE := true
+
+
+# Allow fallback to ashmem
+TARGET_GRALLOC_USES_ASHMEM 	:= true
+TARGET_USES_GENLOCK 		:= true
+TARGET_USES_C2D_COMPOSITION 	:= false
+
+## Qualcomm
+# camera
+BOARD_CAMERA_USE_GETBUFFERINFO := true
+COMMON_GLOBAL_CFLAGS += -DNO_RGBX_8888 -DBINDER_COMPAT
+COMMON_GLOBAL_CFLAGS += -DSEMC_ICS_CAMERA_BLOB
+COMMON_GLOBAL_CFLAGS += -DQCOM_LEGACY_OMX
+
+# audio (prevent breakage from QCOM_HARDWARE in system/audio.h)
+TARGET_PROVIDES_LIBAUDIO := true
+BOARD_USES_AUDIO_LEGACY  := false
+
+# GPS
+BOARD_GPS_LIBRARIES := libgps librpc
+BOARD_USES_GPSSHIM  := true
+
+# bluetooth
+BOARD_HAVE_BLUETOOTH 	 := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Sensors
+TARGET_USES_OLD_LIBSENSORS_HAL := true
+
+# touchscreen
+BOARD_USE_SKIA_LCDTEXT := true
+BOARD_USE_NEW_LIBRIL_HTC := true
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+# trackball
+BOARD_USE_LEGACY_TRACKPAD := true
+
+# Qcom specifics
+BOARD_VENDOR_USE_AKMD := akm8973
+BOARD_VENDOR_QCOM_AMSS_VERSION := 1355
+TARGET_NO_HW_VSYNC := true
+TARGET_DISABLE_TRIPLE_BUFFERING := true
+TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+
+## web
+TARGET_WEBKIT_USE_MORE_MEMORY := true
+JS_ENGINE := v8
+HTTP := chrome
+
+WITH_JIT := true
+ENABLE_JSC_JIT := true
+
+# # cat /proc/mtd
+# dev:    size   erasesize  name
+# mtd0: 000a0000 00020000 "misc"
+# mtd1: 00420000 00020000 "recovery"
+# mtd2: 002c0000 00020000 "boot"
+# mtd3: 0fa00000 00020000 "system"
+# mtd4: 02800000 00020000 "cache"
+# mtd5: 0af20000 00020000 "userdata"
+# Changed for Buzz
+BOARD_BOOTIMAGE_PARTITION_SIZE 		:= 2883584
+BOARD_RECOVERYIMAGE_PARTITION_SIZE 	:= 4325376
+BOARD_SYSTEMIMAGE_PARTITION_SIZE 	:= 262144000
+BOARD_USERDATAIMAGE_PARTITION_SIZE 	:= 183631872
+BOARD_FLASH_BLOCK_SIZE 			:= 131072
+
+# Usb mouting
+BOARD_UMS_LUNFILE 		:= "/sys/devices/platform/usb_mass_storage/lun0/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 320
+TARGET_SCREEN_WIDTH := 240
+
+# FM radio support
+BOARD_HAVE_FM_RADIO 	:= true
+BOARD_FM_DEVICE 	:= bcm4329
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
--- cm.mk
+++ cm.mk
@@ -1,18 +1,74 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := buzz
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Inherit device configuration
-$(call inherit-product, device/htc/buzz/device_buzz.mk)
+$(call inherit-product, $(LOCAL_PATH)/buzz.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := buzz
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Setup device configuration
 PRODUCT_NAME := cm_buzz
-PRODUCT_BRAND := htc
-PRODUCT_MODEL := buzz
-PRODUCT_MANUFACTURER := htc
+PRODUCT_RELEASE_NAME := buzz
+PRODUCT_DEVICE := buzz
+PRODUCT_BRAND := HTC
+PRODUCT_MODEL := Wildfire
+PRODUCT_MANUFACTURER := HTC
+
+#Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.1/JRN84D/392829:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.1 JRN84D 392829 release-key"
+
+# Release name and versioning
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
+TARGET_NO_LIVEWALLPAPERS := true
+
+# RIL specific configuration
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libhtc_ril.so \
+    ro.ril.ecc.HTC-WWE=999 \
+    ro.ril.ecc.HTC-ELL=92,93,94 \
+    ro.ril.enable.a52.HTC-ITA=1 \
+    ro.ril.enable.a53.HTC-ITA=1 \
+    ro.ril.enable.a52=0 \
+    ro.ril.enable.a53=1 \
+    ro.ril.vmail.23415=1571,BT \
+    ro.ril.hsdpa.category = 8 \
+    ro.ril.htcmaskw1.bitmask = 4294967295 \
+    ro.ril.htcmaskw1 = 14449 \
+    ro.ril.def.agps.mode = 2 \
+    ro.ril.gprsclass=12
+
+# For userdebug builds
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.secure=0 \
+    ro.allow.mock.location=1 \
+    ro.debuggable=1
+	
+# Permissions support files
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+
+
